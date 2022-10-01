4255b55e97ac9dc145f01b1da033b2480cdea8e3
easeclaps <92627754+easeclaps@users.noreply.github.com>
2022-10-01 23:17:00 +0100

    Update command.py
    Speed optimisation and command injection vulnerability patch

--- src/CLI/utils/command.py
+++ src/CLI/utils/command.py
@@ -1,11 +1,13 @@
 from subprocess import run, DEVNULL
 from rich.console import Console
 
+import shlex
+
 def execute_command(command: str) -> None:
     if command is None:
         raise ValueError("command can't be empty")
 
-    process = run(command, stdout=DEVNULL, stderr=DEVNULL, shell=True, capture_output=False)
+    process = run(shlex.split(shlex.quote(command)), stdout=DEVNULL, stderr=DEVNULL, shell=True, capture_output=False)
 
     if process.returncode != 0:
         Console().print(" - Failed to run: [bold red]" + command + "[/bold red]")
@@ -14,5 +16,4 @@ def execute_commands(commands: list) -> None:
     if commands is None:
         raise ValueError("commands can't be empty")
 
-    for command in commands:
-        execute_command(command)
+    [execute_command(command) for command in commands]
